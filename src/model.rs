01f944323b56f0267bfb6d4f55f7b52692bb4d41
H1rono <hronok66@gmail.com>
2022-12-29 20:44:00 +0900

    :globe_with_meridians: DateTime to OffsetDateTime for timestamp type

--- src/model.rs
+++ src/model.rs
@@ -1,13 +1,13 @@
-use chrono::{DateTime, Utc};
+use sqlx::types::time::OffsetDateTime;
 
 #[derive(Debug, Clone, PartialEq, Eq, sqlx::FromRow)]
 pub struct Todo {
     pub id: u32,
     pub title: String,
     pub note: String,
-    pub due_to: DateTime<Utc>,
+    pub due_to: OffsetDateTime,
     pub done: i8,
-    pub created_at: DateTime<Utc>,
-    pub updated_at: DateTime<Utc>,
-    pub deleted_at: Option<DateTime<Utc>>,
+    pub created_at: OffsetDateTime,
+    pub updated_at: OffsetDateTime,
+    pub deleted_at: Option<OffsetDateTime>,
 }
