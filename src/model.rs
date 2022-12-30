7e89b99859db240cc038c8deac3b0369bc9da3a4
H1rono <hronok66@gmail.com>
2022-12-31 07:36:24 +0900

    :ambulance: Remove `time` feature of sqlx
    https://github.com/time-rs/time/issues/293

--- src/model.rs
+++ src/model.rs
@@ -1,13 +1,15 @@
-use sqlx::types::time::OffsetDateTime;
+use sqlx::types::chrono::{DateTime, Utc};
+
+pub type TimeStamp = DateTime<Utc>;
 
 #[derive(Debug, Clone, PartialEq, Eq, sqlx::FromRow)]
 pub struct Todo {
     pub id: u32,
     pub title: String,
     pub note: String,
-    pub due_to: OffsetDateTime,
+    pub due_to: TimeStamp,
     pub done: i8,
-    pub created_at: OffsetDateTime,
-    pub updated_at: OffsetDateTime,
-    pub deleted_at: Option<OffsetDateTime>,
+    pub created_at: TimeStamp,
+    pub updated_at: TimeStamp,
+    pub deleted_at: Option<TimeStamp>,
 }
