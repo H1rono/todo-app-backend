a4c867bcb4954de06b2fc329330155fc2aaf74d9
H1rono <hronok66@gmail.com>
2022-12-31 23:57:32 +0900

    :sparkles: Add struct Database

--- src/model.rs
+++ src/model.rs
@@ -1,4 +1,8 @@
-use sqlx::types::chrono::{DateTime, Utc};
+use anyhow::{Context, Result};
+use sqlx::{
+    types::chrono::{DateTime, Utc},
+    FromRow, MySqlPool,
+};
 
 pub type TimeStamp = DateTime<Utc>;
 
@@ -12,4 +16,83 @@ pub struct Todo {
     pub created_at: TimeStamp,
     pub updated_at: TimeStamp,
     pub deleted_at: Option<TimeStamp>,
+}
+
+pub struct Database {
+    pool: MySqlPool,
+}
+
+impl Database {
+    pub async fn connect(url: &str) -> Result<Self> {
+        let pool = MySqlPool::connect(url)
+            .await
+            .with_context(|| format!("an error occured while connecting to database {}", url))?;
+        Ok(Self { pool })
+    }
+
+    pub async fn fetch_all_todos(&self) -> Result<Vec<Todo>> {
+        let todos = sqlx::query("SELECT `id`, `title`, `note`, `due_to`, `created_at`, `done`, `created_at`, `updated_at`, `deleted_at` FROM `todos`")
+            .fetch_all(&self.pool)
+            .await
+            .context("An error occured while executing SELECT query")?;
+        let mut res = vec![];
+        for todo in todos {
+            let todo = Todo::from_row(&todo)
+                .with_context(|| format!("Failed to parse fetched row {:?}", todo))?;
+            res.push(todo);
+        }
+        Ok(res)
+    }
+
+    pub async fn fetch_todo_by_id(&self, id: u32) -> Result<Todo> {
+        let todo = sqlx::query("SELECT `id`, `title`, `note`, `due_to`, `created_at`, `done`, `created_at`, `updated_at`, `deleted_at` FROM `todos` WHERE `id` == ? LIMIT 1")
+            .bind(id)
+            .fetch_one(&self.pool)
+            .await
+            .with_context(|| format!("Failed to fetch a todo by id {}", id))?;
+        Todo::from_row(&todo).with_context(|| format!("Failed to parse fetched row {:?}", todo))
+    }
+
+    pub async fn insert_todo(&self, todo: Todo) -> Result<()> {
+        let Todo {
+            id,
+            title,
+            note,
+            due_to,
+            done,
+            created_at,
+            updated_at,
+            deleted_at,
+        } = todo.clone();
+        sqlx::query("INSERT INTO `todos` (`id`, `title`, `note`, `due_to`, `done`, `created_at`, `updated_at`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")
+            .bind(id)
+            .bind(title)
+            .bind(note)
+            .bind(due_to)
+            .bind(done)
+            .bind(created_at)
+            .bind(updated_at)
+            .bind(deleted_at)
+            .execute(&self.pool)
+            .await
+            .with_context(|| format!("An error occured while INSERTing a todo {:?}", todo))?;
+        Ok(())
+    }
+
+    pub async fn insert_partial_todo(
+        &self,
+        title: String,
+        due_to: TimeStamp,
+        note: Option<String>,
+    ) -> Result<()> {
+        let note = note.unwrap_or_default();
+        sqlx::query("INSERT INTO `todos` (`title`, `note`, `due_to`) VALUES (?, ?, ?)")
+            .bind(title.clone())
+            .bind(due_to)
+            .bind(note.clone())
+            .execute(&self.pool)
+            .await
+            .with_context(|| format!("An error occured while INSERTing a todo with title=`{}`, due_to=`{}`, note=`{}`", title, due_to.to_string(), note))?;
+        Ok(())
+    }
 }
