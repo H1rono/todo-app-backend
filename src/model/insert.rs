1f7bd907598ef8b77b8b12ad13a4fc892a727715
H1rono <hronok66@gmail.com>
2023-02-16 22:24:00 +0900

    :fire: Remove Database::insert_todo

--- src/model/insert.rs
+++ src/model/insert.rs
@@ -1,6 +1,6 @@
 use anyhow::Context;
 
-use super::{Database, PartialTodo, Result, Todo};
+use super::{Database, PartialTodo, Result};
 
 fn try_cvt(n: u64) -> Result<u32> {
     let res = n
@@ -10,35 +10,6 @@ fn try_cvt(n: u64) -> Result<u32> {
 }
 
 impl Database {
-    pub async fn insert_todo(&self, todo: &Todo) -> Result<u32> {
-        let Todo {
-            id,
-            title,
-            note,
-            due_to,
-            done,
-            created_at,
-            updated_at,
-            deleted_at,
-        } = todo;
-        let id = sqlx::query(
-            "INSERT INTO `todos` (`id`, `title`, `note`, `due_to`, `done`, `created_at`, `updated_at`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
-        )
-        .bind(id)
-        .bind(title)
-        .bind(note)
-        .bind(due_to)
-        .bind(done)
-        .bind(created_at)
-        .bind(updated_at)
-        .bind(deleted_at)
-        .execute(&self.pool)
-        .await
-        .with_context(|| format!("Failed to INSERT a todo {todo:?}"))?
-        .last_insert_id();
-        try_cvt(id)
-    }
-
     pub async fn insert_partial_todo(&self, todo: &PartialTodo) -> Result<u32> {
         let PartialTodo {
             title,
