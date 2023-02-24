ccc8750b5aa66379ee68491db41d5ce59bbd11bc
H1rono <hronok66@gmail.com>
2023-02-25 02:31:59 +0900

    :truck: Rename app to app_router

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,14 @@ async fn main() -> Result<()> {
     let url =
         env::var("DATABASE_URL").context("Failed to get environment variable DATABASE_URL")?;
     let db = Database::connect(&url).await?;
-    let app = make_router(db);
+    let app_router = make_router(db);
     let port = env::var("PORT").context("Failed to get environment variable PORT")?;
     let port: u16 = port
         .parse()
         .with_context(|| format!("Cannot parse \"{port}\" as u16"))?;
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+        .serve(app_router.into_make_service())
         .await
         .context("Something went wrong while serving")?;
     Ok(())
