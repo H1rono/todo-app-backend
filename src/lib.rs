25213c508317645d9dafc4496bc8c64842a9e3f7
H1rono <hronok66@gmail.com>
2023-12-07 12:18:03 +0900

    :arrow_up: Bump dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub mod router;
 use std::net::SocketAddr;
 
 use anyhow::{Context, Result};
+use tokio::net::TcpListener;
 
 use config::Config;
 use model::Database;
@@ -19,8 +20,10 @@ pub async fn start(conf: Config) -> Result<()> {
     let db = Database::connect(&database_url).await?;
     let app_router = make_router(db, allowed_origin.parse()?);
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
-    axum::Server::bind(&addr)
-        .serve(app_router.into_make_service())
+    let tcp_listener = TcpListener::bind(addr)
+        .await
+        .with_context(|| format!("failed to bind address {addr}"))?;
+    axum::serve(tcp_listener, app_router)
         .await
         .context("Something went wrong while serving")?;
     Ok(())
